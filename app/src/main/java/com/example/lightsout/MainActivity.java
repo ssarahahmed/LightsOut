36c29ca755d6fa7c991ba294ee0db5952805fa4b
ssarahahmed <sarahaahmed13@gmail.com>
2025-09-13 17:30:28 -0500

    Added new score text view and a method that counts how many lights are on, and linked it to the string resource to.

--- app/src/main/java/com/example/lightsout/MainActivity.java
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.lightsout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int GRID_SIZE = 3;
     private GridLayout grid;
     private boolean cellState[][];
+    private TextView lightsCountTextView;
+
 
     View.OnClickListener buttonListener = new View.OnClickListener() {
         @Override
@@ -33,53 +38,76 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 recolor();
+                updateLightsCount();
             }
         }
     };
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-            cellState = new boolean[][]{{true, true, true}, {true, true, true}, {true, true, true}};
+        cellState = new boolean[][]{{true, true, true}, {true, true, true}, {true, true, true}};
 
-            setContentView(R.layout.activity_main);
-            grid = findViewById(R.id.light_grid);
+        setContentView(R.layout.activity_main);
+        grid = findViewById(R.id.light_grid);
 
-            randomize();
+        lightsCountTextView = findViewById(R.id.lights_count);
 
-            recolor();
+        randomize();
 
-            for (int i = 0; i < grid.getChildCount(); i++) {
-                Button curButton = (Button) grid.getChildAt(i);
-                curButton.setOnClickListener(buttonListener);
-            }
+        recolor();
+        updateLightsCount();
+        Log.d("LightsOut", "Lights on: " + countLightsOn());
+
+
+        for (int i = 0; i < grid.getChildCount(); i++) {
+            Button curButton = (Button) grid.getChildAt(i);
+            curButton.setOnClickListener(buttonListener);
         }
+    }
 
-        public void recolor() {
-            for (int i = 0; i < grid.getChildCount(); i++) {
-                Button gridButton = (Button) grid.getChildAt(i);
+    public void recolor() {
+        for (int i = 0; i < grid.getChildCount(); i++) {
+            Button gridButton = (Button) grid.getChildAt(i);
 
-                // Find the button's row and col
-                int row = i / GRID_SIZE;
-                int col = i % GRID_SIZE;
+            // Find the button's row and col
+            int row = i / GRID_SIZE;
+            int col = i % GRID_SIZE;
 
-                if (cellState[row][col] == true) {
-                    gridButton.setBackgroundColor(getColor(R.color.blue_500));
-                } else {
-                    gridButton.setBackgroundColor(getColor(R.color.black));
-                }
-            }
-        }
-
-        public void randomize() {
-            Random random = new Random();
-            for (int i = 0; i < GRID_SIZE; i++) {
-                for (int j = 0; j < GRID_SIZE; j++) {
-                    cellState[i][j] = random.nextBoolean();
-                }
+            if (cellState[row][col] == true) {
+                gridButton.setBackgroundColor(getColor(R.color.blue_500));
+            } else {
+                gridButton.setBackgroundColor(getColor(R.color.black));
             }
         }
     }
+
+    public void randomize() {
+        Random random = new Random();
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                cellState[i][j] = random.nextBoolean();
+            }
+        }
+    }
+
+
+    public int countLightsOn() {
+        int count = 0;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (cellState[i][j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void updateLightsCount() {
+        int lightsOn = countLightsOn();
+        lightsCountTextView.setText("Lights on: " + lightsOn);
+    }
+}
 
 
